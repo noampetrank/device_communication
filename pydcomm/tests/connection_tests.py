0bfe1eb1002d221346e2f274e4727aa77d2bacba
Ziv Peleg <zivpeleg@gmail.com>
2018-08-20 16:39:32 +0300

    Add benchmark

---
+++ pydcomm/tests/connection_tests.py
@@ -0,0 +1,55 @@
+import time
+from unittest import TestCase
+
+"""
+TODO:
+# Benchmark table
+#   Wired
+#       No recovery
+#       With recovery
+#   Wireless
+#       No recovery - from ip
+#       No recovery - from device
+#       With recovery - from ip
+#       With recovery - from device
+#   
+#   Long run
+#   Many short connections
+#   Adb shell multiple times
+Mock tests
+  Failing tests
+Real tests
+  Stress tests
+"""
+
+
+def test_connection_is_working(connection):
+    return connection.adb("shell echo hi") == "hi"
+
+
+def test_connection_and_disconnect(connection):
+    if not test_connection_is_working(connection):
+        connection.disconnect()
+        return False
+    connection.disconnect()
+    if test_connection_is_working(connection):
+        raise Exception("Could not disconnect")
+
+
+def repeat_test_connection(connect_from_ip, recovery, rounds, sleep_between_connections):
+    connection_factory = ConnectionFactory()
+    # User intervention
+    connection = connection_factory.get_connected(auto_recovery=recovery)
+    ip = connection.get_connection_status()["ip"]
+    connection.disconnect()
+    fail = 0
+    for i in range(rounds):
+        if connect_from_ip:
+            connection = Connection(ip)
+        else:
+            connection = connection_factory.get_connected(auto_recovery=recovery)
+        fail += test_connection_and_disconnect(connection)
+        time.sleep(sleep_between_connections)
+    return fail, rounds
+
+
