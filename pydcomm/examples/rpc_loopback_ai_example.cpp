e978e32e0354383856ec35c76009eacefa701ed2
Michael Litvin <michael.litvin@gmail.com>
2018-08-28 16:15:46 +0300

    Defines RPC API except streaming
    WIP API
    
    More API polishing + usage example
    
    More RPC API polishing during review with Dror
    
    More RPC API polishing following review with Dror
    
    completed rpc_benchmarks; added passing expected func ability in time_it in benchmark_utils.
    
    Renamed IRemoteProcedureSetup to IRemoteProcedureServer

---
+++ pydcomm/examples/rpc_loopback_ai_example.cpp
@@ -0,0 +1,38 @@
+#include "remote_procedure_execution.h"
+
+// Noam
+class LoopbackAIProcedureExecutor : public StandardRemoteProcedureExecutor {
+public:
+    virtual void onStart() {} // Connection established, do whatever initialization is needed after that.
+    virtual void onStop() {} // Connection is about to close.
+
+    virtual std::string getVersion() { return "0.0"; }
+
+    // Unmarshalls the relevant params, runs the procedure, marshalls the returned params and returns them.
+    virtual MarshalledObject onExecuteProcedure(std::string procedureName const MarshalledObject &params) {
+        if (procedureName == 'record_and_play') {
+            auto p = unmarshall<RecordAndPlayParams>(params);
+            play(p.song);
+            AudioBuffer rec = record();
+            return marshall(rec)
+        }
+    }
+};
+
+
+
+// Kadosh
+class IRemoteProcedureServer {
+public:
+    virtual void listen(IRemoteProcedureExecutor &listener) {
+        registerIntent();
+    }
+}
+
+
+void main() {
+    //Noam
+    LoopbackAIProcedureExecutor exec;
+    IRemoteProcedureServer server();
+    server.listen(exec);
+}
---
+++ pydcomm/remote_procedure_call.py
@@ -0,0 +1,70 @@
+class IRemoteProcedureCaller:
+    def call(self, procedure_name, params, marshaller=None, unmarshaller=None):
+        """
+        Marshalls the params and sends them to the executor side. Then receives params that are returned from the executor and unmarshalls them.
+        :type procedure_name: str
+        :type params: marshallable
+        :param params: Can be any type marshallable by the marshaller, possibly generators that will be converted to streams on the executor.
+        :type marshaller: function or None
+        :type unmarshaller: function or None
+        :return: Unmarshalled returned params (i.e. float, dict, object etc.)
+        :rtype: dict[str: object]
+        """
+        pass
+
+    def get_executor_version(self):
+        """
+        Returns the version string of the executor.
+        :rtype: str
+        """
+
+    def start(self):
+        """
+        Do whatever it takes to make the remote executor listen.
+        :rtype: None
+        """
+
+    def stop(self):
+        """
+        Put the remote executor to bed.
+        :rtype: None
+        """
+        pass
+
+
+class StandardRemoteProcedureCaller(IRemoteProcedureCaller):
+    def call(self, procedure_name, params, marshaller=None, unmarshaller=None):
+        marshalled_params = marshaller(params) if marshaller else str(params)
+        ret = self._send_and_wait_for_return(procedure_name, marshalled_params)
+        return unmarshaller(ret) if unmarshaller else ret
+
+    def get_executor_version(self):
+        return self._send_and_wait_for_return('_rpc_get_version', {})
+
+    def start(self):
+        # ...
+        xver = self.get_executor_version()
+        # check if we can handle this executor version
+
+    def stop(self):
+        pass
+
+    def _send_and_wait_for_return(self, procedure_name, marshalled_params):
+        """
+        Specific implementation that calls a procedure on the executor and returns values from it.
+        :param procedure_name:
+        :param marshalled_params:
+        :return:
+        """
+        raise NotImplementedError('This method needs to be overridden by subclass')
+
+
+class UnsupportedExecutorVersion(Exception):
+    pass
+
+
+class ExecutorConnectionError(Exception):
+    pass
+
+
+# TODO add streaming API
