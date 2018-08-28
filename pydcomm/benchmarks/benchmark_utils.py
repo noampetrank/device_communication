73022e9e70e12e82ba8dd5e451c58548c5359667
noharh <nohar@bugatone.com>
2018-08-28 11:07:36 +0300

    exprot reusable code to benchmark utils

---
+++ pydcomm/benchmarks/benchmark_utils.py
@@ -0,0 +1,96 @@
+import time
+import pandas as pd
+
+
+def time_it(obj, method, args, expected=None):
+    """
+    run a method on the object and time it.
+    todo: test this
+    :type expected: list
+    :type obj: object
+    :type method: str
+    :type args: list
+    :rtype: tuple<bool, float>
+    """
+    start = time.time()
+    ok = True
+    try:
+        res = getattr(obj, method)(*args)
+        # if we got expected return value(s) check against them
+        ok = True if expected is None else res in expected
+    except:
+        ok = False
+    finally:
+        return ok, (time.time() - start)*1000.0
+
+
+def benchmark_it(repeats, obj, method, args, expected=None):
+    """
+    time a method repeately, get time and success code for each run
+    :type repeats: repeats
+    :type expected: list
+    :type obj: object
+    :type method: str
+    :type args: list
+    :rtype: list<tuple<bool, float>>
+    """
+    res = []
+    for i in range(repeats):
+        res.append(time_it(obj, method, args, expected=expected))
+    return res
+
+
+def print_table(summaries):
+    """
+    load the summary into pandas and print it
+    :type summaries: list<dict>
+    :return:
+    """
+    columns = 'test_name speed_avg_ms speed_min_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total'.split()
+    data = []
+    for s in summaries:
+        data.append([s["test_name"],
+                     s["speed_average_ms"],
+                     s["speed_min_ms"],
+                     s["speed_max_ms"],
+                     s["n_pass"],
+                     s["pass_rate"],
+                     s["n_fail"],
+                     s["fail_rate"],
+                     s["n_total"]])
+
+    df = pd.DataFrame(data,columns=columns).set_index(columns[0])
+    print(df)
+
+
+def get_stats(name, single_benchmark):
+    """
+    get stats for a benchmark
+    :type name: str
+    :type single_benchmark: list<tuple<bool, float>>
+    :rtype: dict
+    """
+    times = [n[1] for n in single_benchmark]
+    success = [n[0] for n in single_benchmark]
+    n_ok = len([n for n in success if n is True])
+    total = len(single_benchmark)
+    return {
+        "test_name": name,
+        "speed_average_ms": np.average(times),
+        "speed_min_ms": min(times),
+        "speed_max_ms": max(times),
+        "n_total": total,
+        "n_pass": n_ok,
+        "pass_rate": float(n_ok)/total,
+        "n_fail": total - n_ok,
+        "fail_rate": float(total - n_ok)/total,
+    }
+
+
+def create_summary(benchmark_results):
+    """
+    create a summary of the results and print them
+    :type benchmark_results: dict<list<tuple<bool, float>>>
+    """
+    summaries = [get_stats(key, benchmark_results[key]) for key in benchmark_results.keys()]
+    print_table(summaries)
--- pydcomm/benchmarks/device_utils_benchmarks.py
+++ pydcomm/benchmarks/device_utils_benchmarks.py
@@ -70,98 +70,9 @@ get_prop                      10.0         25.3        208.1    198      0.99   
 set_prop                      10.0         25.3        208.1    198      0.99      2      0.01     200
 is_earphone_connected         10.0         25.3        208.1    198      0.99      2      0.01     200
 is_max_volume                 10.0         25.3        208.1    198      0.99      2      0.01     200
-
-
-
-
-========================== Demo code: ==========================
-columns='test_name speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total'.split()
-
-def table1(name):
-    data = []
-    data.append((name+'_empty_file 10.0 25.3 208.1 198 0.99 2 0.01 200').split())
-    data.append((name+'_1kB 12.0 27.3 238.1 98 0.98 2 0.02 100').split())
-    data.append((name+'_1MB 100.0 250.3 2080.1 7 0.7 3 0.3 10').split())
-    data.append((name+'_100MB 1000.0 2500.3 20800.1 2 0.67 1 0.33 3').split())
-    data.append((name+'_1GB nan nan nan 0 0.0 1 1.0 1').split())
-    df = pd.DataFrame(data,columns=columns).set_index(columns[0])
-    print(df)
-
-# push
-print('Push file:')
-table1('push')
-
-# pull
-print('\n\n\nPull file:')
-table1('pull')
-
-# push+pull
-print('\n\n\nPush+pull file:')
-table1('push_pull')
-
-# get/set_volume
-def append_row(data, name):
-    data.append((name+' 10.0 25.3 208.1 198 0.99 2 0.01 200').split())
-data = []
-for name in ['volume_' + str(x) for x in range(17)]:
-    append_row(data, name)
-df = pd.DataFrame(data,columns=columns).set_index(columns[0])
-print('\n\n\nSet volume:')
-print(df)
-
-
-
-# the rest (except reboot)
-def append_row(data, name):
-    data.append((name+' 10.0 25.3 208.1 198 0.99 2 0.01 200').split())
-data = []
-for name in ('send_intent', 'mkdir', 'touch_file', 'ls', 'get_time', 'remove', 'get_device_name', 'get_prop', 'set_prop', 'is_earphone_connected', 'is_max_volume'):
-    append_row(data, name)
-df = pd.DataFrame(data,columns=columns).set_index(columns[0])
-print('\n\n\nOther DeviceUtils methods:')
-print(df)
 """
-import time
-import pandas as pd
 import os
-import numpy as np
-
-
-def time_it(device, method, args, expected=None):
-    """
-    run a method on the device and time it.
-    todo: test this
-    :type expected: list
-    :type device: DeviceUtils
-    :type method: str
-    :type args: list
-    :rtype: bool, float
-    """
-    start = time.time()
-    ok = True
-    try:
-        res = getattr(device, method)(*args)
-        # if we got expected return value(s) check against them
-        ok = True if expected is None else res in expected
-    except:
-        ok = False
-    finally:
-        return ok, time.time() - start
-
-
-def benchmark_it(repeats, device, method, args, expected=None):
-    """
-
-    :param device:
-    :param method:
-    :param args:
-    :param repeats:
-    :return:
-    """
-    res = []
-    for i in range(repeats):
-        res.append(time_it(device, method, args, expected=expected))
-    return res
+from pydcomm.benchmarks.benchmark_utils import time_it, benchmark_it, create_summary
 
 
 def benchmark_dir_funcs(repeats, device):
@@ -194,58 +105,6 @@ def benchmark_touch_remove(repeats, device):
         res_touch.append(time_it(device, "touch", args, expected=[True]))
         res_remove.append(time_it(device, "remove", args, expected=[True]))
     return res_touch, res_remove
-
-
-def print_table(summaries):
-    columns = 'test_name speed_avg_ms speed_min_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total'.split()
-    data = []
-    for s in summaries:
-        data.append([s["test_name"],
-                     s["speed_average_ms"],
-                     s["speed_min_ms"],
-                     s["speed_max_ms"],
-                     s["n_pass"],
-                     s["pass_rate"],
-                     s["n_fail"],
-                     s["fail_rate"],
-                     s["n_total"]])
-
-    df = pd.DataFrame(data,columns=columns).set_index(columns[0])
-    print(df)
-
-
-def get_stats(name, single_benchmark):
-    """
-
-    :param name:
-    :param single_benchmark:
-    :return:
-    """
-    times = [n[1] for n in single_benchmark]
-    success = [n[0] for n in single_benchmark]
-    n_ok = len([n for n in success if n is True])
-    total = len(single_benchmark)
-    return {
-        "test_name": name,
-        "speed_average_ms": np.average(times),
-        "speed_min_ms": min(times),
-        "speed_max_ms": max(times),
-        "n_total": total,
-        "n_pass": n_ok,
-        "pass_rate": float(n_ok)/total,
-        "n_fail": total - n_ok,
-        "fail_rate": float(total - n_ok)/total,
-    }
-
-
-def create_summary(benchmark_results):
-    """
-    create a summary of the results and print them
-    :param benchmark_results:
-    :return:
-    """
-    summaries = [get_stats(key, benchmark_results[key]) for key in benchmark_results.keys()]
-    print_table(summaries)
 
 
 def benchmark_push_pull(repeats, device, size_bytes):
