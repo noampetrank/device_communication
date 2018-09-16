1469e3c84d33d911c72f04a2a1348cd830b082a9
Ziv Peleg <ziv@bugatone.com>
2018-09-16 17:44:06 +0300

    Add nonworking tests for lsusb fixer

--- pydcomm/tests/connection/connection_fixers_tests.py
+++ pydcomm/tests/connection/connection_fixers_tests.py
@@ -1,9 +1,10 @@
 import unittest
 
+from StringIO import StringIO
 import mock
 from nose_parameterized import parameterized
 
-from pydcomm.general_android.connection.connection_fixers import add_adb_recovery_decorator
+from pydcomm.general_android.connection.connection_fixers import add_adb_recovery_decorator, add_no_device_connected_recovery
 
 
 class AddDecoratorTests(unittest.TestCase):
@@ -20,9 +21,10 @@ class AddDecoratorTests(unittest.TestCase):
         a.adb(a, "hi")
         self.assertTrue(d["success"])
 
+    # For some reason I couldn't get hte tests running with just one parameter.
     @parameterized.expand([
-        [True,True],
-        [False,True]
+        [True, True],
+        [False, True]
     ])
     def test_add_adb_recovery_decorator__test_connection_fails__original_adb_is_called(self, connection_success, bla):
         d = {"adb_called": False}
@@ -41,6 +43,38 @@ class AddDecoratorTests(unittest.TestCase):
         self.assertTrue(d["adb_called"])
 
 
-class NoDeviceConnectedRecoveryTests(unittest.TestCase):
-    def test__no_device_disconnected__no_message_shown(self):
+class TestConnection(object):
+    def __init__(self, device_id=None):
+        self.connection_good = True
         pass
+
+    def adb(self, *params):
+        pass
+
+    def test_connection(self):
+        return self.connection_good
+
+
+class NoDeviceConnectedRecoveryTests(unittest.TestCase):
+#     @mock.patch("pydcomm.general_android.connection.connection_fixers.subprocess.check_output")
+#     # https://stackoverflow.com/a/31171719/365408
+#     @mock.patch("pydcomm.general_android.connection.connection_fixers.sys.stdout", new_callable=StringIO)
+#
+#     def test__no_device_disconnected__no_message_shown(self, mock_check_output):
+#         # arrange
+#         mock_connection_cls = TestConnection
+#         mock_connection_cls = add_no_device_connected_recovery(mock_connection_cls)
+#         mock_check_output.return_value = """Bus 002 Device 001: ID 1d6b:0003 Linux Foundation 3.0 root hub
+# Bus 001 Device 003: ID 8087:0a2b Intel Corp.
+# Bus 001 Device 002: ID 046d:c534 Logitech, Inc. Unifying Receiver
+# Bus 001 Device 004: ID 1bcf:2b96 Sunplus Innovation Technology Inc.
+# Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
+# """
+#
+#         # act
+#         mock_connection = mock_connection_cls()
+#         mock_connection.connection_good = False
+#         mock_connection.adb("hi")
+#
+#         # assert
+    # TODO: Get back to this when Dror is here and ask about testing prints and raw_inputs.
