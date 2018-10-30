13e4bbef60a1f07fc2fa0b0900215a3eabe4884a
Ziv Peleg <zivpeleg@gmail.com>
2018-10-30 20:34:13 +0200

    Make sure that the device is on the same network as the computer when connecting wirelessly.

--- pydcomm/tests/connection/adb_connection_tests.py
+++ pydcomm/tests/connection/adb_connection_tests.py
@@ -1,3 +1,7 @@
+import StringIO
+import __builtin__
+import sys
+
 import subprocess32 as subprocess
 import unittest
 import mock
@@ -86,7 +90,9 @@ class GetDeviceIpTests(unittest.TestCase):
         mock_check_output.return_value = ifconfig
         self.assertIsNone(get_device_ip("avocado"))
 
+
 WIRELESS_MODULE_NAME = "pydcomm.general_android.connection.wireless_adb_connection"
+
 
 class WirelessAdbConnectionTests(unittest.TestCase):
     def assert_raises_with_message(self, message):
@@ -95,9 +101,9 @@ class WirelessAdbConnectionTests(unittest.TestCase):
         self.assertIn(message, e.exception.message)
 
     def setUp(self):
-        self.patcher =TestCasePatcher(self)
+        self.patcher = TestCasePatcher(self)
         self.mock_get_device_ip = self.patcher.addPatch(WIRELESS_MODULE_NAME + ".get_device_ip")
-        self.mock_get_device_ip.return_value = "10.0.0.101"
+        self.mock_get_device_ip.return_value = u"10.0.0.101"
         self.mock_check_call = self.patcher.addPatch(WIRELESS_MODULE_NAME + ".subprocess.check_call")
         self.mock_raw_input = self.patcher.addPatch(WIRELESS_MODULE_NAME + ".raw_input")
         self.mock_wired_connection = mock.Mock()
@@ -128,3 +134,24 @@ class WirelessAdbConnectionTests(unittest.TestCase):
     def test_connect__cant_connect_ip__raise_exception(self):
         self.mock_check_call.side_effect = self.raise_exeception_if_correct_command("adb connect 10.0.0.101:5555".split(" "))
         self.assert_raises_with_message("10.0.0.101")
+
+    @mock.patch(WIRELESS_MODULE_NAME + ".get_connected_interfaces_and_addresses")
+    @mock.patch.object(__builtin__, 'raw_input')
+    def test_connect__device_is_not_on_same_network__user_is_asked_to_change_network(self, mock_raw_input, mock_get_interfaces):
+        mock_get_interfaces.return_value = [(u'enx000acd2b99a2',
+                                             [{'addr': u'10.0.0.107',
+                                               'broadcast': u'10.0.0.255',
+                                               'netmask': u'255.255.255.0'}]),
+                                            (u'docker0',
+                                             [{'addr': u'172.17.0.1',
+                                               'broadcast': u'172.17.255.255',
+                                               'netmask': u'255.255.0.0'}])]
+        self.mock_get_device_ip.side_effect = [u"192.168.1.1", u"10.0.0.104"]
+
+        capturedOutput = StringIO.StringIO()
+        sys.stdout = capturedOutput
+
+        connect_wireless(self.mock_wired_connection, None)
+
+        sys.stdout = sys.__stdout__
+        self.assertIn("Device is not connected to the same network as the computer, please change and press enter", capturedOutput.getvalue())
