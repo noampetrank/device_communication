78a6d1f9e9b0783d7c8a6aa545c89cf6933d2ff0
Ziv Peleg <zivpeleg@gmail.com>
2018-10-30 20:34:13 +0200

    Make sure that the device is on the same network as the computer when connecting wirelessly.

--- pydcomm/general_android/connection/connection_fixers.py
+++ pydcomm/general_android/connection/connection_fixers.py
@@ -48,4 +48,27 @@ def get_connected_interfaces_and_addresses():
     interfaces_and_addresses = [x for x in interfaces_and_addresses if x[1] is not None and x[1][0]["addr"] != u"127.0.0.1"]
     return interfaces_and_addresses
 
+
+def get_connected_interfaces_and_addresses():
+    """
+    Returns connected network interfaces - non loopback and that have an IP address.
+    Example output:
+    ```
+    [(u'enx000acd2b99a2',
+      [{'addr': u'10.0.0.107',
+        'broadcast': u'10.0.0.255',
+        'netmask': u'255.255.255.0'}]),
+     (u'docker0',
+      [{'addr': u'172.17.0.1',
+        'broadcast': u'172.17.255.255',
+        'netmask': u'255.255.0.0'}])]
+    ```
+
+    :return: List of tuples of interface name and addresses
+    :rtype: list[tuple[string, list[dict]]]
+    """
+    interfaces_and_addresses = [(x, netifaces.ifaddresses(x).get(netifaces.AF_INET)) for x in netifaces.interfaces()]
+    interfaces_and_addresses = [x for x in interfaces_and_addresses if x[1] is not None and x[1][0]["addr"] != u"127.0.0.1"]
+    return interfaces_and_addresses
+
 # endregion Manual fixes
