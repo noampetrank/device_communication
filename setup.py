88dc765d25850730376101e604058ec76d544ba0
Yotam Karmon <yotam@bugatone.com>
2018-10-31 13:05:26 +0200

    Add ipaddress to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name="pydcomm",
     description="Bugatone's infrastructure for communication with android devices",
     py_modules=['pydcomm'],
-    install_requires=['mock', 'nose', 'parameterized', 'timeout-decorator', 'subprocess32', 'netifaces']
+    install_requires=['mock', 'nose', 'parameterized', 'timeout-decorator', 'subprocess32', 'netifaces', 'ipaddress']
 )
