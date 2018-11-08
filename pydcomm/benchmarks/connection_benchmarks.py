ea745af41f543b6aceb7b769058c12ebdd82d369
Dror Speiser <dror.mastershin@gmail.com>
2018-11-08 16:12:39 +0200

    benchmark thing

--- pydcomm/benchmarks/connection_benchmarks.py
+++ pydcomm/benchmarks/connection_benchmarks.py
@@ -1,150 +1,254 @@
-"""
-========================== Format: ==========================
-Push file:
-                speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total
-test_name
-push_empty_file         10.0         25.3        208.1    198      0.99      2      0.01     200
-push_1kB                12.0         27.3        238.1     98      0.98      2      0.02     100
-push_1MB               100.0        250.3       2080.1      7       0.7      3       0.3      10
-push_100MB            1000.0       2500.3      20800.1      2      0.67      1      0.33       3
-push_1GB                 nan          nan          nan      0       0.0      1       1.0       1
+import pickle
+import time
+import sys
+from traceback import print_exc
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import subprocess32
+from enum import IntEnum
+from pybuga.tests.utils.test_helpers import Tee
+from pydcomm.general_android.connection.connection import DummyBugaConnection, ConnectionError, ConnectionClosedError
+
+from pydcomm.benchmarks.raw_input_recorder import RawInputRecorder
 
 
-
-Pull file:
-                speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total
-test_name
-pull_empty_file         10.0         25.3        208.1    198      0.99      2      0.01     200
-pull_1kB                12.0         27.3        238.1     98      0.98      2      0.02     100
-pull_1MB               100.0        250.3       2080.1      7       0.7      3       0.3      10
-pull_100MB            1000.0       2500.3      20800.1      2      0.67      1      0.33       3
-pull_1GB                 nan          nan          nan      0       0.0      1       1.0       1
+class Recovery(IntEnum):
+    NONE = 0
+    AUTO = 1
+    INTERACTIVE = 2
 
 
-
-Push+pull file:
-                     speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total
-test_name
-push_pull_empty_file         10.0         25.3        208.1    198      0.99      2      0.01     200
-push_pull_1kB                12.0         27.3        238.1     98      0.98      2      0.02     100
-push_pull_1MB               100.0        250.3       2080.1      7       0.7      3       0.3      10
-push_pull_100MB            1000.0       2500.3      20800.1      2      0.67      1      0.33       3
-push_pull_1GB                 nan          nan          nan      0       0.0      1       1.0       1
+ApiCallData = namedtuple("ApiCallData", "type total_time success manual_interactions_count manual_times failure_reason")
 
 
+class ConnectionBenchmark(object):
+    def __init__(self, connection_factory):
+        self.connection_factory = connection_factory
 
-Set volume:
-          speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total
-test_name
-volume_0          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_1          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_2          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_3          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_4          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_5          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_6          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_7          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_8          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_9          10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_10         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_11         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_12         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_13         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_14         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_15         10.0         25.3        208.1    198      0.99      2      0.01     200
-volume_16         10.0         25.3        208.1    198      0.99      2      0.01     200
+        self.stats = []
+        """@type: list[ApiCallData]"""
+
+    def benchmark_connect(self):
+        connection = None
+        failure_reason = None
+        start_time = time.time()
+        input_recorder = RawInputRecorder(ignore_first=True)
+
+        try:
+            with input_recorder:
+                connection = self.connection_factory()
+        except Exception as e:
+            print(e.message)
+            failure_reason = e
+            raise
+        finally:
+            end_time = time.time()
+            self.stats.append(ApiCallData("connect",  # type
+                                          end_time - start_time,  # time
+                                          connection is not None,  # success
+                                          len(input_recorder.input_times),  # manual_interactions_count
+                                          input_recorder.input_times,  # manual_times
+                                          failure_reason))  # failure_reason
+            return connection
+
+    def benchmark_shell(self, connection):
+        success = False
+        failure_reason = None
+        start_time = time.time()
+        input_recorder = RawInputRecorder(ignore_first=True)
+
+        try:
+            with input_recorder:
+                success = connection.shell("echo hi") == "hi"
+        except Exception as e:
+            print(e.message)
+            failure_reason = e
+        finally:
+            end_time = time.time()
+            self.stats.append(ApiCallData("shell",  # type
+                                          end_time - start_time,  # time
+                                          success,  # success
+                                          len(input_recorder.input_times),  # manual_interactions_count
+                                          input_recorder.input_times,  # manual_times
+                                          failure_reason))  # failure_reason
+            return success
+
+    def benchmark_disconnect(self, connection):
+        """
+        :param pydcomm.general_android.connection.Connection connection: Connection to disconnect.
+        :return:
+        """
+        success = False
+        failure_reason = None
+        end_time = None
+        start_time = time.time()
+
+        try:
+            connection.disconnect()
+            end_time = time.time()
+
+            # Expect that adb raises exception
+            try:
+                connection.disconnect()
+            except ConnectionClosedError:
+                success = True
+        except Exception as e:
+            failure_reason = e
+        finally:
+            if end_time is None:
+                end_time = time.time()
+            self.stats.append(ApiCallData("disconnect",  # type
+                                          end_time - start_time,  # time
+                                          success,  # success
+                                          0,  # manual_interactions_count
+                                          [],  # manual_times
+                                          failure_reason))  # failure_reason
+            return connection
+
+    def run(self, num_connection_checks, check_interval):
+        try:
+            # Connect
+            print("Connecting...")  # TODO: remove
+            connection = self.benchmark_connect()
+
+            # Check if connection is alive
+            for i in range(num_connection_checks):
+                print("adb...")  # TODO: remove
+                self.benchmark_shell(connection)
+                print("adb done")  # TODO: remove
+                time.sleep(check_interval)
+
+            # Disconnect
+            print("Disconnecting...")  # TODO: remove
+            self.benchmark_disconnect(connection)
+            print("Disconnecting done")  # TODO: remove
+        except Exception as e:
+            print("Got exception. Aborting this run. Details:")
+            print_exc(e)
 
 
+class TestDefinition(object):
+    def __init__(self, rounds, num_connection_checks, check_interval):
+        self.rounds = rounds
+        self.num_connection_checks = num_connection_checks
+        self.check_interval = check_interval
 
-Other AndroidDeviceUtils methods:
-                      speed_min_ms speed_avg_ms speed_max_ms n_pass pass_rate n_fail fail_rate n_total
-test_name
-send_intent                   10.0         25.3        208.1    198      0.99      2      0.01     200
-mkdir                         10.0         25.3        208.1    198      0.99      2      0.01     200
-touch_file                    10.0         25.3        208.1    198      0.99      2      0.01     200
-ls                            10.0         25.3        208.1    198      0.99      2      0.01     200
-get_time                      10.0         25.3        208.1    198      0.99      2      0.01     200
-remove                        10.0         25.3        208.1    198      0.99      2      0.01     200
-get_device_name               10.0         25.3        208.1    198      0.99      2      0.01     200
-get_prop                      10.0         25.3        208.1    198      0.99      2      0.01     200
-set_prop                      10.0         25.3        208.1    198      0.99      2      0.01     200
-is_earphone_connected         10.0         25.3        208.1    198      0.99      2      0.01     200
-is_max_volume                 10.0         25.3        208.1    198      0.99      2      0.01     200
-"""
-import os
-
-from pydcomm.general_android.connection.connection import DummyBugaConnection
-
-from pydcomm.benchmarks.benchmark_utils import time_it, benchmark_it, create_summary
-from tqdm import tqdm
+    def __str__(self):
+        # Temporary
+        return "{}x{}x{}".format(self.rounds, self.num_connection_checks, self.check_interval)
+        # return "{} <num_connection_checks={}, check_interval={}>".format(self.__class__.__name__,
+        # self.num_connection_checks, self.check_interval)
 
 
-def benchmark_push_pull(repeats, connection, size_bytes, use_tqdm=False):
-    """
-    :param repeats:
-    :param connection:
-    :param size_bytes: size of random data or the random data itself
-    :param bool use_tqdm: Whether or not to show a progress bar.
-    :return:
-    """
-    if type(size_bytes) == str:
-        data = size_bytes
-        size_bytes = len(data)
-    else:
-        data = os.urandom(size_bytes)
+class TestResult(object):
+    def __init__(self, rounds, test_definition, success_count, timeout_exceptions, adb_connection_errors,
+                 total_manual_fix_count, median_fix_time):
+        self.rounds = rounds
+        self.test_definition = test_definition
+        self.success_count = success_count
+        self.timeout_exceptions = timeout_exceptions
+        self.adb_connection_errors = adb_connection_errors
+        self.total_manual_fix_count = total_manual_fix_count
+        self.median_fix_time = median_fix_time
 
-    # create the file on the computer
-    pc_path = "/tmp/tmp_file_%d" % size_bytes
-    with open(pc_path, 'w') as f:
-        f.write(data)
-
-    device_path = "/sdcard/Documents/test_push_%d" % size_bytes
-    push_args = [pc_path, device_path]
-    pull_args = [device_path, pc_path]
-    res_push = []
-    res_pull = []
-    if use_tqdm:
-        iters = tqdm(range(repeats), desc='Iteration', position=1)
-    else:
-        iters = range(repeats)
-    for _ in iters:
-        res_push.append(time_it(connection, "push", push_args, [True]))
-        res_pull.append(time_it(connection, "pull", pull_args, [True]))
-
-    # cleanup
-    os.remove(pc_path)
-    connection.shell("rm %s" % device_path)
-
-    return res_push, res_pull
+    def __repr__(self):
+        return "{}, {}".format(type(self).__name__, self.__dict__)
 
 
-def all_benchmarks(connection, repeats):
-    """
-    benchmark of all methods on device_utils for a specific device
-    :type connection:
-    :type repeats: int
-    :return: None
-    """
-    # benchmark for push and pull files
-    file_size_bytes = [0, 1000, 100000, 200000]  # todo: profile what we really need
+tests = [
+    # rounds, num_connection_checks, check_interval
+    TestDefinition(20, 5, 0),
+    TestDefinition(7, 2, 20),
+    TestDefinition(20, 1, 1)
+]
 
-    push_results = {}
-    pull_results = {}
-    push_pull_results = {}
-    for size in file_size_bytes:
-        push_results["push_%d" % size], pull_results["pull_%d" % size] = benchmark_push_pull(repeats, connection, size)
-    # create and print summary
-    create_summary(dict(push_results, **pull_results))
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
+def print_table(test_results_summed):
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+
+    columns = [
+        "rounds",
+        "test_definition",
+        "success_count",
+        "timeout_exceptions",
+        "adb_connection_errors",
+        "total_manual_fix_count",
+        "median_fix_time",
+    ]
+    data = []
+    for t in test_results_summed:
+        row = [getattr(t, c) for c in columns]
+        data.append(row)
+    df = pd.DataFrame(data, columns=columns).set_index(columns[0])
+    print (df)
+
+
+def create_run_summary(runs):
+    test_results_summed = []
+    for i, run_result in enumerate(runs):
+        flat = flatten(run_result)
+        rounds = len(flat)
+        success_count = len([x for x in flat if x.success])
+        timeout_exceptions = len([x for x in flat if type(x.failure_reason) is subprocess32.TimeoutExpired])
+        adb_connection_error = len([x for x in flat if type(x.failure_reason) is ConnectionError])
+        total_manual_fix_count = sum([x.manual_interactions_count for x in flat])
+        flat_manual_times = flatten([x.manual_times for x in flat])
+        median_fix_time = np.median(flat_manual_times) if flat_manual_times else None
+        result = TestResult(rounds, tests[i], success_count, timeout_exceptions, adb_connection_error,
+                            total_manual_fix_count, median_fix_time)
+        test_results_summed.append(result)
+    return test_results_summed
+
+
+def run_rounds(connection_factory):
+    runs = []
+    try:
+        for test in tests:
+            run_results = []
+            for i in range(test.rounds):
+                b = ConnectionBenchmark(connection_factory)
+                b.run(test.num_connection_checks, test.check_interval)
+                run_results.append(b.stats)
+            runs.append(run_results)
+    except KeyboardInterrupt:
+        pass
+    return runs
+
+
+# TODO: Extract code from pybuga to someplace else
+class BetterTee(Tee):
+    def __enter__(self):
+        self.old_stdout = sys.stdout
+        sys.stdout = self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self.old_stdout
 
 
 def main():
-    connection = DummyBugaConnection()
+    from pybuga.infra.utils.buga_utils import indent_printing
+    start_time_string = time.strftime("%H:%M:%S")
+    with BetterTee("run_{}.log".format(start_time_string)):
+        with indent_printing.time():
+            # from functools import partial
+            # from pydcomm.general_android.connection.connection_factory import AdbConnectionFactory
+            # connection_factory = partial(AdbConnectionFactory.get_oppo_wireless_device, use_manual_fixes=True)
+            connection_factory = DummyBugaConnection
+            time.sleep = lambda _: None
 
-    import pandas as pd
-    pd.set_option("display.max_columns", 20)
-    pd.set_option("display.width", 20000)
-    all_benchmarks(connection, 1)
+            runs = run_rounds(connection_factory)
+            with open("raw_data_" + start_time_string + ".pickle", "w") as f:
+                pickle.dump(runs, f)
+            test_results_summed = create_run_summary(runs)
+            print_table(test_results_summed)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
