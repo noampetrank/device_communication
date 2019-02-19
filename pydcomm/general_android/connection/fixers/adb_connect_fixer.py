52f3447c4e6e1959f24a678654948007ed8f03f1
Yotam Karmon <yotam@bugatone.com>
2019-02-19 12:09:42 +0200

    Kill server before connecting to wireless ADB

--- pydcomm/general_android/connection/fixers/adb_connect_fixer.py
+++ pydcomm/general_android/connection/fixers/adb_connect_fixer.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
+
+from pydcomm.general_android.connection.connection_fixers import restart_adb_server_fix
 from pydcomm.general_android.connection.wired_adb_connection import ConnectingError
-from pydcomm.general_android.connection.wireless_adb_connection import connect_to_wireless_adb
+from pydcomm.general_android.connection.wireless_adb_connection import adb_connect
 
 
 def adb_connect_fix(connection):
     try:
         print("Trying to connect to device")
-        connect_to_wireless_adb(connection,"")
+        restart_adb_server_fix(connection)
+        adb_connect(connection)
     except ConnectingError:
         print("Failed to connect to device")
-        pass
