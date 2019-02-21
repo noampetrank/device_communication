5ac976c9033b4f7d1fea8de853dfc771494cc1a6
Yotam Karmon <yotam@bugatone.com>
2019-02-21 11:55:21 +0200

    Reconnect to wireless in every 'call a developer' fix iteration

--- pydcomm/general_android/connection/fixers/call_a_developer_fixer.py
+++ pydcomm/general_android/connection/fixers/call_a_developer_fixer.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 from pybuga.infra.utils.user_input import UserInput
 
+from pydcomm.general_android.connection.fixers.adb_connect_fixer import adb_connect_fix
+
 
 def call_a_developer_fix(connection):
     print("Please call a developer!!!")
@@ -10,4 +12,6 @@ def call_a_developer_fix(connection):
     while not connection_fixed:
         if not UserInput.yes_no("Do you want to try again? [y/n]"):
             break
+        if not connection.wired:
+            adb_connect_fix(connection)
         connection_fixed = connection.test_connection()
