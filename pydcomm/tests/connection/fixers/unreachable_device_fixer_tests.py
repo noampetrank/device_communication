2f6c57b7d194de49a84f88d687d094af55cb3b4f
Yotam Karmon <yotam@bugatone.com>
2019-02-28 14:46:05 +0200

    Fix unreachable device fixer tests

--- pydcomm/tests/connection/fixers/unreachable_device_fixer_tests.py
+++ pydcomm/tests/connection/fixers/unreachable_device_fixer_tests.py
@@ -12,26 +12,26 @@ class UnreachableDeviceFixerTests(unittest.TestCase):
         self.connection = mock.Mock()
         self.connection.device_id = u"10.0.0.104:5555"
 
-    @mock.patch(TESTED_MODULE_NAME + ".connect_to_wireless_adb")
+    @mock.patch(TESTED_MODULE_NAME + ".adb_connect_fix")
     @mock.patch(TESTED_MODULE_NAME + ".subprocess.call")
     @mock.patch(TESTED_MODULE_NAME + ".query_yes_no")
     def test_unreachable_device_fix__device_connected__user_is_not_prompted_no_reconnect(self, mock_query_yes_no,
                                                                                          mock_call,
-                                                                                         mock_connect_to_wireless_adb):
+                                                                                         mock_adb_connect_fix):
         mock_call.return_value = 0
 
         with MockStdout() as mock_stdout:
             unreachable_device_fix(self.connection)
             self._verify_message_not_printed_to_user(mock_stdout.getvalue())
             mock_query_yes_no.assert_not_called()
-            mock_connect_to_wireless_adb.assert_not_called()
+            mock_adb_connect_fix.assert_not_called()
 
-    @mock.patch(TESTED_MODULE_NAME + ".connect_to_wireless_adb")
+    @mock.patch(TESTED_MODULE_NAME + ".adb_connect_fix")
     @mock.patch(TESTED_MODULE_NAME + ".subprocess.call")
     @mock.patch(TESTED_MODULE_NAME + ".query_yes_no")
     def test_unreachable_device_fix__happy_path__user_is_prompted_and_reconnect(self, mock_query_yes_no,
                                                                                 mock_call,
-                                                                                mock_connect_to_wireless_adb):
+                                                                                mock_adb_connect_fix):
         mock_call.side_effect = [1, 1, 0]  # First call: fail. Second call: success
         mock_query_yes_no.return_value = True
         mock_query_yes_no_call = mock.call("Did you reconnect the device?")
@@ -40,14 +40,14 @@ class UnreachableDeviceFixerTests(unittest.TestCase):
             unreachable_device_fix(self.connection)
             self._verify_message_printed_to_user(mock_stdout.getvalue())
             mock_query_yes_no.assert_has_calls([mock_query_yes_no_call, mock_query_yes_no_call])
-            mock_connect_to_wireless_adb.assert_called()
+            mock_adb_connect_fix.assert_called()
 
-    @mock.patch(TESTED_MODULE_NAME + ".connect_to_wireless_adb")
+    @mock.patch(TESTED_MODULE_NAME + ".adb_connect_fix")
     @mock.patch(TESTED_MODULE_NAME + ".subprocess.call")
     @mock.patch(TESTED_MODULE_NAME + ".query_yes_no")
     def test_unreachable_device_fix__user_gave_up__user_is_prompted_no_connection(self, mock_query_yes_no,
                                                                                   mock_call,
-                                                                                  mock_connect_to_wireless_adb):
+                                                                                  mock_adb_connect_fix):
         mock_call.return_value = 1
         mock_query_yes_no.side_effect = [True, True, False]
         mock_query_yes_no_call = mock.call("Did you reconnect the device?")
@@ -56,27 +56,27 @@ class UnreachableDeviceFixerTests(unittest.TestCase):
             unreachable_device_fix(self.connection)
             self._verify_message_printed_to_user(mock_stdout.getvalue())
             mock_query_yes_no.assert_has_calls([mock_query_yes_no_call, mock_query_yes_no_call, mock_query_yes_no_call])
-            mock_connect_to_wireless_adb.assert_not_called()
+            mock_adb_connect_fix.assert_not_called()
 
-    @mock.patch(TESTED_MODULE_NAME + ".connect_to_wireless_adb")
+    @mock.patch(TESTED_MODULE_NAME + ".adb_connect_fix")
     @mock.patch(TESTED_MODULE_NAME + ".subprocess.call")
     @mock.patch(TESTED_MODULE_NAME + ".query_yes_no")
     def test_unreachable_device_fix__initial_ping_success__nothing_is_done(self, mock_query_yes_no,
                                                                            mock_call,
-                                                                           mock_connect_to_wireless_adb):
+                                                                           mock_adb_connect_fix):
         mock_call.return_value = 0
 
         with MockStdout() as mock_stdout:
             unreachable_device_fix(self.connection)
             self._verify_message_not_printed_to_user(mock_stdout.getvalue())
             mock_query_yes_no.assert_not_called()
-            mock_connect_to_wireless_adb.assert_not_called()
+            mock_adb_connect_fix.assert_not_called()
 
-    @mock.patch(TESTED_MODULE_NAME + ".connect_to_wireless_adb")
+    @mock.patch(TESTED_MODULE_NAME + ".adb_connect_fix")
     @mock.patch(TESTED_MODULE_NAME + ".subprocess.call")
     @mock.patch(TESTED_MODULE_NAME + ".query_yes_no")
     def test_unreachable_device_fix__connection_is_wired__no_ping_done(self, mock_query_yes_no, mock_call,
-                                                                       mock_connect_to_wireless_adb):
+                                                                       mock_adb_connect_fix):
         self.connection.device_id = u"BH9003KE51"
 
         with MockStdout() as mock_stdout:
@@ -85,7 +85,7 @@ class UnreachableDeviceFixerTests(unittest.TestCase):
 
         mock_call.assert_not_called()
         mock_query_yes_no.assert_not_called()
-        mock_connect_to_wireless_adb.assert_not_called()
+        mock_adb_connect_fix.assert_not_called()
 
     def _verify_message_printed_to_user(self, stdout):
         self.assertIn("Device is not reachable.", stdout)
