bf14fd7db3c562a5e8f19d35c1feffa07bb29300
noharh <nohar@bugatone.com>
2019-03-18 11:50:01 +0200

    adding play_song and delay

--- pydcomm/public/deviceutils/media_player_utils.py
+++ pydcomm/public/deviceutils/media_player_utils.py
@@ -1,9 +1,14 @@
+import os
+import time
+
+from pydcomm.rpc.buga_grpc_client import GRpcLibbugatoneAndroidClientFactory
+
 
 class MediaPlayerUtils(object):
     """
     class for controlling a media player
     """
-    def __init__(self, connection, activity="com.chahal.mpc.hd/org.videolan.vlc.StartActivity"):
+    def __init__(self, connection, activity="com.chahal.mpc.hd/org.videolan.vlc.StartActivity", delay=None):
         """
         
         :param IConnection connection: connection to a device
@@ -12,13 +17,17 @@ class MediaPlayerUtils(object):
         super(MediaPlayerUtils, self).__init__()
         self.connection = connection
         self.activity = activity
+        self.delay = delay
 
     def _send_key(self, keyid):
         """
         :param int keyid:
         """
         self.connection.shell("input keyevent " + str(keyid), timeout_ms=3000)
-        
+        # delay script (by sleeping) in an attempt to avoid media player crashes / red screens
+        if self.delay:
+            time.sleep(self.delay)
+
     def media_player_select_song(self, device_song_path):
         """
         open a song in the default media player on the device
@@ -28,6 +37,9 @@ class MediaPlayerUtils(object):
         media_cmd = 'am start -a android.intent.action.VIEW -d file://%s -t audio/wav --user 0%s' % (
             device_song_path, (" -n " + self.activity) if self.activity is not None else "")
         self.connection.shell(media_cmd)
+        # delay script (by sleeping) in an attempt to avoid media player crashes / red screens
+        if self.delay:
+            time.sleep(self.delay)
         self.media_player_rewind_song()  # so it play from the beginning
     
     def media_player_rewind_song(self):
@@ -65,3 +77,18 @@ class MediaPlayerUtils(object):
         Send the play button event
         """
         self._send_key(126)
+
+    def _delay_for_media_player(self):
+        # delay script (by sleeping) in an attempt to avoid media player crashes / red screens
+        if self.delay:
+            time.sleep(self.delay)
+
+    def play_song(self, song, path=GRpcLibbugatoneAndroidClientFactory.DEVICE_MUSIC_PATH):
+        """
+        player this song on the default activity
+        :param str song: name of song file
+        :param str path: path to songs on the device
+        :return:
+        """
+        device_path = os.path.join(path, song)
+        self.media_player_select_song(device_path)
