7be08526ba7a156d1557d701ea5c33304cc41640
Yotam Karmon <yotam@bugatone.com>
2019-11-21 13:11:55 +0200

    Explicitly call play() after selecting song in external player

--- pydcomm/public/deviceutils/media_player_utils.py
+++ pydcomm/public/deviceutils/media_player_utils.py
@@ -144,6 +144,7 @@ class MediaPlayerUtils(IMediaPlayerUtils):
         """
         device_path = os.path.join(path, song)
         self.media_player_select_song(device_path)
+        self.media_player_play_song()
 
 
 class LinuxVlcMediaPlayerUtils(IMediaPlayerUtils):
